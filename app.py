4ef9cbdec0dd8cdaf6866cf8f0c53ad7ea0bf8ad
Thiago Cacio <94044199+NihilLeaf@users.noreply.github.com>
2022-07-31 13:14:40 -0300

    lanchers launching fireworks!

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import random
 
 pygame.init()
 #Setting the window config
-WIDTH, HEIGHT = 600, 400
+WIDTH, HEIGHT = 800, 600
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('FIREWORKS')
 
@@ -21,17 +21,45 @@ COLORS = [
     (255, 192, 203),
 ]
 #Class of projectiles
-class projects:
+class projectiles:
     pass
 
 
 #The firework class
 class fireworks:
-    pass
+    RADIUS = 10
+    MAX_PROJECTILES = 50
+    MIN_PROJECTILES = 25
+    PROJECTILE_V = 4
 
+    def __init__(self, x, y, y_v, explode_height, color):
+        self.x = x
+        self.y = y
+        self.y_v = y_v
+        self.explode_height = explode_height
+        self.color = color
+        self.projectiles = []
+        self.exploded = False
+    
+    def explode(self):
+        self.exploded = True
+
+    def move(self, max_width, max_height):
+        if not self.exploded:
+            self.y += self.y_v
+            if self.y <= self.explode_height:
+                self.explode()
+
+    
+    def draw(self, win):
+        if not self.exploded:
+            pygame.draw.circle(win, self.color, (self.x, self.y), self.RADIUS)
+
+        for projectile in self.projectiles:
+            projectile.draw(win)
 
 #creating the launchers class
-class launcher:
+class Launcher:
     WIDTH = 20
     HEIGHT = 20
     COLOR = 'grey'
@@ -47,8 +75,14 @@ class launcher:
     def draw(self, win):
         pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.WIDTH, self.HEIGHT))
 
+        for firework in self.fireworks:
+            firework.draw(win)
+
     def launch(self):
-        pass
+        color = random.choice(COLORS)
+        explode_height = random.randrange(50, 400)
+        firework = fireworks(self.x + self.WIDTH / 2, self.y, -5 , explode_height, color)
+        self.fireworks.append(firework)
 
     def loop(self, max_width, max_height):
         current_time = time.time()
@@ -57,11 +91,22 @@ class launcher:
         if time_elapsed * 1000 >= self.frequency:
             self.start_time = current_time
             self.launch()
+
+        fireworks_to_remove = []
+        for firework in self.fireworks:
+            firework.move(max_width, max_height)
+            if firework.exploded and len(str(firework.projectiles == 0)):
+                fireworks_to_remove.append(firework)
+        
+        for firework in fireworks_to_remove:
+            self.fireworks.remove(firework)
+ 
+
 #deffing color of background
 def draw(launchers):
     win.fill('black')
-    for launcher in launchers:
-        launcher.draw(win)
+    for Launcher in launchers:
+        Launcher.draw(win)
     pygame.display.update()
 
 
@@ -69,7 +114,7 @@ def draw(launchers):
 def main():
     run = True
     clock = pygame.time.Clock()
-    launchers = [launcher(100, HEIGHT - launcher.HEIGHT, random.randint(2000, 4001))]
+    launchers = [Launcher(100, HEIGHT - Launcher.HEIGHT, random.randint(2000, 4001))]
 
     #Run the program in 60 FPS
     while run:
@@ -80,6 +125,10 @@ def main():
                 run = False
                 break
         draw(launchers)
+
+        for launcher in launchers:
+            launcher.loop(WIDTH, HEIGHT)
+
     pygame.quit()
     quit()
 
