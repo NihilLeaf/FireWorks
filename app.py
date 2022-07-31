7fc99201b08b0687fa9feb85a81dc41b45ab35e0
Thiago Cacio <94044199+NihilLeaf@users.noreply.github.com>
2022-07-31 12:16:30 -0300

    1 launcher adcionado!

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from ast import While
 import pygame
+import time
+import random
+
 pygame.init()
 #Setting the window config
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 600, 400
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('FIREWORKS')
 
@@ -18,27 +20,68 @@ COLORS = [
     (230, 230, 255),
     (255, 192, 203),
 ]
+#Class of projectiles
+class projects:
+    pass
 
+
+#The firework class
+class fireworks:
+    pass
+
+
+#creating the launchers class
+class launcher:
+    WIDTH = 20
+    HEIGHT = 20
+    COLOR = 'grey'
+
+    def __init__(self, x, y, frequency):
+        self.x = x
+        self.y = y
+        self.frequency = frequency #mileseconds
+        self.start_time = time.time()
+        self.fireworks = []
+    
+    #Fuction to Draw the launchers
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.WIDTH, self.HEIGHT))
+
+    def launch(self):
+        pass
+
+    def loop(self, max_width, max_height):
+        current_time = time.time()
+        time_elapsed = current_time - self.start_time
+
+        if time_elapsed * 1000 >= self.frequency:
+            self.start_time = current_time
+            self.launch()
 #deffing color of background
-def draw():
+def draw(launchers):
     win.fill('black')
+    for launcher in launchers:
+        launcher.draw(win)
     pygame.display.update()
+
 
 #Starting the loop to the window
 def main():
     run = True
     clock = pygame.time.Clock()
+    launchers = [launcher(100, HEIGHT - launcher.HEIGHT, random.randint(2000, 4001))]
+
     #Run the program in 60 FPS
     while run:
         clock.tick(FPS)
-    
     #Checking the events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-            break
-    draw()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+        draw(launchers)
     pygame.quit()
     quit()
+
 if __name__ == '__main__':
     main()
